0fe667775d38193a96726e6ad2f6ea8162acf35e
drab-dev <reubynngeorge7x@gmail.com>
2025-09-29 13:48:53 +0530

    supabase stuff

---
+++ supabase/migrations/20250929080150_remote_schema.sql
@@ -0,0 +1,309 @@
+revoke delete on table "public"."domains" from "anon";
+
+revoke insert on table "public"."domains" from "anon";
+
+revoke references on table "public"."domains" from "anon";
+
+revoke select on table "public"."domains" from "anon";
+
+revoke trigger on table "public"."domains" from "anon";
+
+revoke truncate on table "public"."domains" from "anon";
+
+revoke update on table "public"."domains" from "anon";
+
+revoke delete on table "public"."domains" from "authenticated";
+
+revoke insert on table "public"."domains" from "authenticated";
+
+revoke references on table "public"."domains" from "authenticated";
+
+revoke select on table "public"."domains" from "authenticated";
+
+revoke trigger on table "public"."domains" from "authenticated";
+
+revoke truncate on table "public"."domains" from "authenticated";
+
+revoke update on table "public"."domains" from "authenticated";
+
+revoke delete on table "public"."domains" from "service_role";
+
+revoke insert on table "public"."domains" from "service_role";
+
+revoke references on table "public"."domains" from "service_role";
+
+revoke select on table "public"."domains" from "service_role";
+
+revoke trigger on table "public"."domains" from "service_role";
+
+revoke truncate on table "public"."domains" from "service_role";
+
+revoke update on table "public"."domains" from "service_role";
+
+revoke delete on table "public"."group_members" from "anon";
+
+revoke insert on table "public"."group_members" from "anon";
+
+revoke references on table "public"."group_members" from "anon";
+
+revoke select on table "public"."group_members" from "anon";
+
+revoke trigger on table "public"."group_members" from "anon";
+
+revoke truncate on table "public"."group_members" from "anon";
+
+revoke update on table "public"."group_members" from "anon";
+
+revoke delete on table "public"."group_members" from "authenticated";
+
+revoke insert on table "public"."group_members" from "authenticated";
+
+revoke references on table "public"."group_members" from "authenticated";
+
+revoke select on table "public"."group_members" from "authenticated";
+
+revoke trigger on table "public"."group_members" from "authenticated";
+
+revoke truncate on table "public"."group_members" from "authenticated";
+
+revoke update on table "public"."group_members" from "authenticated";
+
+revoke delete on table "public"."group_members" from "service_role";
+
+revoke insert on table "public"."group_members" from "service_role";
+
+revoke references on table "public"."group_members" from "service_role";
+
+revoke select on table "public"."group_members" from "service_role";
+
+revoke trigger on table "public"."group_members" from "service_role";
+
+revoke truncate on table "public"."group_members" from "service_role";
+
+revoke update on table "public"."group_members" from "service_role";
+
+revoke delete on table "public"."group_whiteboard_shares" from "anon";
+
+revoke insert on table "public"."group_whiteboard_shares" from "anon";
+
+revoke references on table "public"."group_whiteboard_shares" from "anon";
+
+revoke select on table "public"."group_whiteboard_shares" from "anon";
+
+revoke trigger on table "public"."group_whiteboard_shares" from "anon";
+
+revoke truncate on table "public"."group_whiteboard_shares" from "anon";
+
+revoke update on table "public"."group_whiteboard_shares" from "anon";
+
+revoke delete on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke insert on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke references on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke select on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke trigger on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke truncate on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke update on table "public"."group_whiteboard_shares" from "authenticated";
+
+revoke delete on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke insert on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke references on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke select on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke trigger on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke truncate on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke update on table "public"."group_whiteboard_shares" from "service_role";
+
+revoke delete on table "public"."groups" from "anon";
+
+revoke insert on table "public"."groups" from "anon";
+
+revoke references on table "public"."groups" from "anon";
+
+revoke select on table "public"."groups" from "anon";
+
+revoke trigger on table "public"."groups" from "anon";
+
+revoke truncate on table "public"."groups" from "anon";
+
+revoke update on table "public"."groups" from "anon";
+
+revoke delete on table "public"."groups" from "authenticated";
+
+revoke insert on table "public"."groups" from "authenticated";
+
+revoke references on table "public"."groups" from "authenticated";
+
+revoke select on table "public"."groups" from "authenticated";
+
+revoke trigger on table "public"."groups" from "authenticated";
+
+revoke truncate on table "public"."groups" from "authenticated";
+
+revoke update on table "public"."groups" from "authenticated";
+
+revoke delete on table "public"."groups" from "service_role";
+
+revoke insert on table "public"."groups" from "service_role";
+
+revoke references on table "public"."groups" from "service_role";
+
+revoke select on table "public"."groups" from "service_role";
+
+revoke trigger on table "public"."groups" from "service_role";
+
+revoke truncate on table "public"."groups" from "service_role";
+
+revoke update on table "public"."groups" from "service_role";
+
+revoke delete on table "public"."profiles" from "anon";
+
+revoke insert on table "public"."profiles" from "anon";
+
+revoke references on table "public"."profiles" from "anon";
+
+revoke select on table "public"."profiles" from "anon";
+
+revoke trigger on table "public"."profiles" from "anon";
+
+revoke truncate on table "public"."profiles" from "anon";
+
+revoke update on table "public"."profiles" from "anon";
+
+revoke delete on table "public"."profiles" from "authenticated";
+
+revoke insert on table "public"."profiles" from "authenticated";
+
+revoke references on table "public"."profiles" from "authenticated";
+
+revoke select on table "public"."profiles" from "authenticated";
+
+revoke trigger on table "public"."profiles" from "authenticated";
+
+revoke truncate on table "public"."profiles" from "authenticated";
+
+revoke update on table "public"."profiles" from "authenticated";
+
+revoke delete on table "public"."profiles" from "service_role";
+
+revoke insert on table "public"."profiles" from "service_role";
+
+revoke references on table "public"."profiles" from "service_role";
+
+revoke select on table "public"."profiles" from "service_role";
+
+revoke trigger on table "public"."profiles" from "service_role";
+
+revoke truncate on table "public"."profiles" from "service_role";
+
+revoke update on table "public"."profiles" from "service_role";
+
+revoke delete on table "public"."whiteboard_shares" from "anon";
+
+revoke insert on table "public"."whiteboard_shares" from "anon";
+
+revoke references on table "public"."whiteboard_shares" from "anon";
+
+revoke select on table "public"."whiteboard_shares" from "anon";
+
+revoke trigger on table "public"."whiteboard_shares" from "anon";
+
+revoke truncate on table "public"."whiteboard_shares" from "anon";
+
+revoke update on table "public"."whiteboard_shares" from "anon";
+
+revoke delete on table "public"."whiteboard_shares" from "authenticated";
+
+revoke insert on table "public"."whiteboard_shares" from "authenticated";
+
+revoke references on table "public"."whiteboard_shares" from "authenticated";
+
+revoke select on table "public"."whiteboard_shares" from "authenticated";
+
+revoke trigger on table "public"."whiteboard_shares" from "authenticated";
+
+revoke truncate on table "public"."whiteboard_shares" from "authenticated";
+
+revoke update on table "public"."whiteboard_shares" from "authenticated";
+
+revoke delete on table "public"."whiteboard_shares" from "service_role";
+
+revoke insert on table "public"."whiteboard_shares" from "service_role";
+
+revoke references on table "public"."whiteboard_shares" from "service_role";
+
+revoke select on table "public"."whiteboard_shares" from "service_role";
+
+revoke trigger on table "public"."whiteboard_shares" from "service_role";
+
+revoke truncate on table "public"."whiteboard_shares" from "service_role";
+
+revoke update on table "public"."whiteboard_shares" from "service_role";
+
+revoke delete on table "public"."whiteboards" from "anon";
+
+revoke insert on table "public"."whiteboards" from "anon";
+
+revoke references on table "public"."whiteboards" from "anon";
+
+revoke select on table "public"."whiteboards" from "anon";
+
+revoke trigger on table "public"."whiteboards" from "anon";
+
+revoke truncate on table "public"."whiteboards" from "anon";
+
+revoke update on table "public"."whiteboards" from "anon";
+
+revoke delete on table "public"."whiteboards" from "authenticated";
+
+revoke insert on table "public"."whiteboards" from "authenticated";
+
+revoke references on table "public"."whiteboards" from "authenticated";
+
+revoke select on table "public"."whiteboards" from "authenticated";
+
+revoke trigger on table "public"."whiteboards" from "authenticated";
+
+revoke truncate on table "public"."whiteboards" from "authenticated";
+
+revoke update on table "public"."whiteboards" from "authenticated";
+
+revoke delete on table "public"."whiteboards" from "service_role";
+
+revoke insert on table "public"."whiteboards" from "service_role";
+
+revoke references on table "public"."whiteboards" from "service_role";
+
+revoke select on table "public"."whiteboards" from "service_role";
+
+revoke trigger on table "public"."whiteboards" from "service_role";
+
+revoke truncate on table "public"."whiteboards" from "service_role";
+
+revoke update on table "public"."whiteboards" from "service_role";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.update_updated_at_column()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  NEW.updated_at = CURRENT_TIMESTAMP;
+  RETURN NEW;
+END;
+$function$
+;
+
+
+
